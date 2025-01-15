6181207b6f1228bcca64fd3633af4fddbda1a139
tdoaftl <twelvedaysoff.and.fivetealatte@gmail.com>
2025-01-15 17:50:50 +0900

    商品登録機能作成

--- app/controllers/clothings_controller.rb
+++ app/controllers/clothings_controller.rb
@@ -1,3 +1,27 @@
 class ClothingsController < ApplicationController
-  
+  def index
+  end
+
+  def new
+    @clothing = Clothing.new
+  end
+
+  def create
+    @clothing = Clothing.new(item_params)
+    if @clothing.save
+    redirect_to root_path
+    else   
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:clothing).permit(
+      :item, :description, :price, :brand, :age, :category,
+      :condition, :size, :color, :material
+    ).merge(user_id: current_user.id)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "clothings#index"
-  resources :clothings, only: :index
+  resources :clothings, only: [:index, :new, :create]
 
     resources :users , only: [:index] do
       collection do
