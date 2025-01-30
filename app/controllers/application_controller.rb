4a591e74fcbd6a02fa11dfb972f35d12f24b4154
tdoaftl <twelvedaysoff.and.fivetealatte@gmail.com>
2025-01-30 11:54:33 +0900

    basic認証設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
@@ -14,4 +15,12 @@ class ApplicationController < ActionController::Base
     end
     super(resource, params)
   end
+
+
+private
+def basic_auth
+  authenticate_or_request_with_http_basic do |username, password|
+    username == ENV["ADMIN_USER"] && password == ENV["ADMIN_PASSWORD"]
+  end
+end
 end
